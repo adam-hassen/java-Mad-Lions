72e02743c70c8b0fcda3c1842866d1a703d2e7e2
adam <adam.hassen@esprit.tn>
2024-05-02 20:15:45 +0100

    Petite Modification

--- src/main/java/Recyclage/controllers/BakTest.java
+++ src/main/java/Recyclage/controllers/BakTest.java
@@ -1,16 +1,22 @@
 package Recyclage.controllers;
 
+import Recyclage.entities.ProduitRecyclable;
+import Recyclage.services.ProduitRecyclableMethodes;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.Map;
 
 public class BakTest {
-
+    @FXML
+    private Button home;
     @FXML
     private Button Affichage;
 
@@ -42,6 +48,14 @@ public class BakTest {
     private Button workshop;
 
     @FXML
+    private HBox hboxButtons;
+
+    @FXML
+    void initialize() {
+        hboxButtons.setVisible(false);
+   
+    }
+    @FXML
     void Afficher(ActionEvent event) throws IOException {
         AnchorPane view= FXMLLoader.load((getClass().getResource("/EcoDepot/AfficherEcoDepot.fxml")));
         BorderPane.setCenter(view);
@@ -57,5 +71,41 @@ public class BakTest {
     void carte(ActionEvent event) throws IOException {
         AnchorPane view= FXMLLoader.load((getClass().getResource("/EcoDepot/Carte.fxml")));
         BorderPane.setCenter(view);
+    }
+    @FXML
+    void Recyclage(ActionEvent event) throws IOException {
+        AnchorPane view= FXMLLoader.load((getClass().getResource("/EcoDepot/AjouterEcoDepot.fxml")));
+        BorderPane.setCenter(view);
+        hboxButtons.setVisible(true);
+    }
+
+    @FXML
+    void Utilisateur(ActionEvent event) throws IOException {
+        AnchorPane view= FXMLLoader.load((getClass().getResource("/EcoDepot/AfficherEcoDepot.fxml")));
+        BorderPane.setCenter(view);
+        hboxButtons.setVisible(false);
+    }
+    @FXML
+    void consommation(ActionEvent event) {
+        hboxButtons.setVisible(false);
+    }
+
+    @FXML
+    void evenemment(ActionEvent event) {
+        hboxButtons.setVisible(false);
+    }
+
+    @FXML
+    void produit(ActionEvent event) {
+        hboxButtons.setVisible(false);
+    }
+
+    @FXML
+    void workshop(ActionEvent event) {
+        hboxButtons.setVisible(false);
+    }
+    @FXML
+    void home(ActionEvent event) {
+
     }
 }
