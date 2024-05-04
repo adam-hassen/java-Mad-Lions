11285aed1d37399ff0f6157997150ffbb3d02af8
adam <adam.hassen@esprit.tn>
2024-05-04 13:54:36 +0100

    User+Recyclage+Consommation

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,37 @@
+module GestionRecyclage {
     requires javafx.graphics;
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.media;
     requires java.sql;
+    requires qrgen;
+    requires com.gluonhq.maps;
     requires javafx.web;
+    requires jdk.jsobject;
+    requires twilio; // Ajout de la bibliothèque net.glxn.qrgen
+    requires com.google.gson;
+    requires java.persistence;
+    requires profanity.filter;
+    requires java.desktop;
+    requires bcrypt;
+    requires org.jfree.jfreechart;
+    requires org.jfree.chart.fx;
+    requires javax.mail.api;
+    requires okhttp3;
+    opens Recyclage.entities;
+    opens Recyclage.controllers;
+    opens Recyclage.services;
+    opens Recyclage.interfaces;
+    opens Recyclage.tests;
+    opens Recyclage.tools;
+    opens org.example.controller;
+    opens org.example.entity to javafx.base;
+    opens org.example.service;
+    opens EDU.userjava1.controllers;
+    opens EDU.userjava1.entities to javafx.base;
+    exports org.example;
+    exports org.example.entity;
+    exports org.example.controller;
+    exports org.example.service;
 
 }
